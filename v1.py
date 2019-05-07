f387bd9b97ebbeb87bdc625175fe9c54f69164a5
rodrigozamb <rodrigo-zamboni@hotmail.com>
2019-05-07 01:53:31 -0300

    botoes + cores

--- v1.py
+++ v1.py
@@ -1,80 +1,116 @@
 import pygame
 import sys
+import primitivas
 
+#inicialização do pygame
 pygame.init()
 screen = pygame.display.set_mode((900, 600))
+screen.fill([255, 255, 255])
+
+#cores
 white = [255, 255, 255]
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
 black = [0,0,0]
+listaCores = [black,red,blue,green,white,(128,0,128),(255,20,147),(255,255,0),(0,255,255),(255,0,255),(128,128,128),(210,105,30),(56,176,222),(1,250,42),(255,215,0),(180,82,205)]
+colorNum = 0
 MenuColor = [200,200,200]
+
+#janela
 width = 900
 height = 600
 size = (width,height)
 
+#variaveis de controle
 mousePos = (0,0) 
-
-
-screen.fill(white)
 botoesMenu=[]
+primitivas = ['Linha','Retângulo','Quadrado','Círculo','Polilinha','Curva'] # relação numerica de cada primitiva
+primitivaNum = 0; 
+
 
 def iniciaMenu():
+	global colorNum
 	pygame.draw.rect(screen,(200,200,200),pygame.Rect(0,0,width,100)) # Menu cinza principal
 
 	aux = (width//30)
 	pygame.draw.rect(screen,black,pygame.Rect((width//30),(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect((width//30),(width//30)+50,50,5))         # Botao Linha
+	pygame.draw.rect(screen,green,pygame.Rect((width//30),(width//30)+50,50,5))         # Botao Linha
 	botoesMenu.append((aux,(width//30),(aux+50),(aux+50)))
 
 	aux = (width//30)+100
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+100,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+200
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+200,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+300
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+300,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+400
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+400,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+500
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+500,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+600
 	pygame.draw.rect(screen,black,pygame.Rect((width//30)+600,(width//30),50,50))     # Botao Externo Linha
-	pygame.draw.rect(screen,red,pygame.Rect(aux,(width//30)+50,50,5))
+	pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
 	aux = (width//30)+700
-	pygame.draw.rect(screen,black,pygame.Rect((width//30)+700,(width//30),50,50))     # Botao Externo Linha
+	pygame.draw.rect(screen,listaCores[colorNum],pygame.Rect((width//30)+700,(width//30),50,50))     # Botao Externo Linha
 	botoesMenu.append((aux,(width//30),(aux+50),(width//30)+50))
 
-
-
 def muda_botao(pos):
+	global primitivaNum
+	global colorNum
 	print(pos)
-	if pos[1]<100:
-		for i in range(len(botoesMenu)):
+	a=-1
+
+
+	for i in range(len(botoesMenu)):
+		
+		if pos[0]>= botoesMenu[i][0] and pos[0]<=botoesMenu[i][2] and pos[1]>=botoesMenu[i][1] and pos[1]<=botoesMenu[i][3]:
+			a=i;
+			primitivaNum = i					
+	
+	if(a==7):
+		print("mudar cor")
+		colorNum+=1
+		if(colorNum == len(listaCores)):
+			colorNum = 0
+
+		aux = (width//30)+700
+		pygame.draw.rect(screen,listaCores[colorNum],pygame.Rect((width//30)+700,(width//30),50,50))     # Botao Externo Linha
+
+	elif (a!=-1):
+		for i in range(len(botoesMenu)-1):
 			aux = botoesMenu[i][0]
-			if pos[0]>= botoesMenu[i][0] and pos[0]<=botoesMenu[i][2] and pos[1]>=botoesMenu[i][1] and pos[1]<=botoesMenu[i][3]:
-					pygame.draw.rect(screen,green,pygame.Rect(aux,(width//30)+50,50,5))
-					print("botao ",i, "mp - ",pos, " posBot - ",botoesMenu[i])
+			if a==i:
+				pygame.draw.rect(screen,green,pygame.Rect(aux,(width//30)+50,50,5))
 			else:
 				pygame.draw.rect(screen,MenuColor,pygame.Rect(aux,(width//30)+50,50,5))
-		pygame.display.update()		
 
+
+	pygame.display.update()		
+
+
+
+
+def click_handle(pos):
+	if(pos[1] <= 100):
+		muda_botao(pos)
 
 
 
@@ -84,14 +120,14 @@ print(botoesMenu)
 
 
 while 1 :
+	mousePos = pygame.mouse.get_pos()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			muda_botao(pygame.mouse.get_pos())
-		pygame.display.update()		
-
+			click_handle(mousePos)
+	
 	pygame.display.update()		
 
 
